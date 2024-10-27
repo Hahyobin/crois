49dfb9fbef0430714c2e7cbb551a7cfb1ec5660f
Hahyobin <129256143+Hahyobin@users.noreply.github.com>
2024-10-28 04:06:08 +0900

    Update test.py

--- test.py
+++ test.py
@@ -235,7 +235,7 @@ def main():
                 font-style: italic;
             }
         </style>
-        <div class="title">Crois Data Mining</div>
+        <div class="title">Data Mining(For Hyundai)</div>
     """, unsafe_allow_html=True)
 
     st.markdown("""<style>.subtitle{text-align:center;font-size:17px;font-style: italic;}</style><div class="subtitle">Data Processing and Analysis Application</div>""", unsafe_allow_html=True)
